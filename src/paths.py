2acda111fab85c6ac6dd8fced6ba0ce45c3e86bf
Carlos Melo <carlos.fab@gmail.com>
2023-11-04 15:07:47 -0300

    Notebook do M01 atualizado

---
+++ src/paths.py
@@ -0,0 +1,19 @@
+# --- Imports ---
+from pathlib import Path
+import os
+
+
+# --- Constants ---
+PARENT_DIR = Path(__file__).parent.resolve().parent
+DATA_DIR = PARENT_DIR / 'data'
+
+
+# --- Functions ---
+def create_dir_if_not_exists(directory: Path) -> None:
+    """Creates the specified directory if it doesn't exist."""
+    if not directory.exists():
+        os.mkdir(directory)
+
+
+# --- Directory Creation ---
+create_dir_if_not_exists(DATA_DIR)
